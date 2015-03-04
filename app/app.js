2ff5735d535b0b438833baf348f00c3d7a0b99a6
Jesse Wolgamott <jesse@comalproductions.com>
2015-03-04 07:16:21 -0800

    Make the magic happen with ember

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
@@ -10,6 +11,11 @@ var App = Ember.Application.extend({
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
+
+DS.RESTAdapter.reopen({
+  namespace: "api",
+  host: "http://seven-seven-seven-api.herokuapp.com"
+})
 
 loadInitializers(App, config.modulePrefix);
 
--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('photo', {path: "photo/:photo_id"});
 });
 
 export default Router;
